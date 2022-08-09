3420cc3576826441502af69366e4b683feafbd0e
Adrian Khaskin <haskin.dev@gmail.com>
2022-08-09 12:25:12 -0400

    added notes

--- src/main/java/dev/haskin/springtestinglab2/Springtestinglab2Application.java
+++ src/main/java/dev/haskin/springtestinglab2/Springtestinglab2Application.java
@@ -5,6 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+/*
+ * Problems:
+ 	- The URL you will need is: http://api.coincap.io/v2/assets/bitcoin - the API
+   			is free to use and does not require authentication.
+ 	x Create an endpoint that uses this API to return the current cost of 1 Bitcoin.
+	x  Make sure each method is unit test and integration tested as appropriate.
+ */
 @SpringBootApplication
 public class Springtestinglab2Application {
 
